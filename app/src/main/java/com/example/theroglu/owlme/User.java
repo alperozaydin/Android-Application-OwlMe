d2ba3b5da2058dbbce256268be1cd66624945624
theroglu <aeroglu2016@gmail.com>
2017-11-25 21:43:58 +0100

    User Profile Class has added

---
+++ app/src/main/java/com/example/theroglu/owlme/User.java
@@ -0,0 +1,95 @@
+package com.example.theroglu.owlme;
+
+import android.media.Image;
+
+import java.util.Date;
+
+/**
+ * Created by theroglu on 25.11.17.
+ */
+
+//this class represents a user class containing all the variables for displaying the required info on the screen.
+
+public class User {
+
+    private String username;
+    private Date birthday;
+    private Image profilePicture;
+    private String bio;
+    private String languages;
+    private int languageLevel;
+    private String country;
+    private String age;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public Image getProfilePicture() {
+        return profilePicture;
+    }
+
+    public void setProfilePicture(Image profilePicture) {
+        this.profilePicture = profilePicture;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+
+    public String getLanguages() {
+        return languages;
+    }
+
+    public void setLanguages(String languages) {
+        this.languages = languages;
+    }
+
+    public int getLanguageLevel() {
+        return languageLevel;
+    }
+
+    public void setLanguageLevel(int languageLevel) {
+        this.languageLevel = languageLevel;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+
+
+
+
+
+
+
+}
